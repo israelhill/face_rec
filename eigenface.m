5302f54000d8edc26345a2f00d81f2c526725ea5
jdl <jdl116@case.edu>
2016-12-14 13:33:38 -0500

    improved eigenface

--- eigenface.m
+++ eigenface.m
@@ -52,6 +52,7 @@ testing_set(:,:,training_idx) = [];
 avg_cropped = mean(training_set,3);
 figure
 imagesc(avg_cropped); colormap gray; title('Average Face');
+imwrite(mat2gray(avg_cropped),'avg_face.png');
 
 %% reshape
 for j = 1:length(training_set)
@@ -60,9 +61,13 @@ end
 avg_cropped_vector = reshape(avg_cropped,[192*168 1]);
 avg_cropped_vector = repmat(avg_cropped_vector,1,size(training_set,3));
 figure
-imagesc(cropped_vector); colormap gray; title('Rearranged Faces');
+imagesc(cropped_vector); colormap gray; %title('Rearranged Faces');
+xlabel('Testing Face'); ylabel('Pixels');
+print(gcf,'rearr_test_face','-dpng','-r300');
+%imwrite(mat2gray(cropped_vector),'rearrface.png');
 figure
 imagesc(avg_cropped_vector); colormap gray; title('Rearranged Average Faces');
+%imwrite(mat2gray(avg_cropped_vector),'rearr_avg_face.png');
 
 %% subtract the mean
 A = single(double(cropped_vector)-avg_cropped_vector);
@@ -71,9 +76,11 @@ A = single(double(cropped_vector)-avg_cropped_vector);
 [U,S,V] = svd(A,'econ');
 
 %% showing eigenfaces
-for j = 0:27
-    subplot(4,7,j+1)
-    imagesc(reshape(U(:,j+1),192,168)); colormap gray
+figure
+for j = 1:6
+    subplot(3,2,j)
+    imagesc(reshape(U(:,j),192,168)); axis off; colormap gray
+    imwrite(mat2gray(reshape(U(:,j),192,168)),strcat('Eigen',num2str(j),'.png'));
 end
 
 %% weights
@@ -81,13 +88,15 @@ end
 figure
 hold on
 for i = 1:size(training_set,3)   
-    for j = 1:13
+    for j = 4:500
         w_training(j) = U(:,j)'*A(:,i);
     end
     omega_training(:,i) = w_training;
     plot(w_training)
 end
 hold off
+xlabel('Eigenface'); ylabel('Weight');
+print(gcf,'training_weight','-dpng','-r300');
 
 %% face recognition
 % find weight of testing set
@@ -100,7 +109,7 @@ A_training = single(A_training);
 figure
 hold on
 for i = 1:size(testing_set,3)   
-    for j = 1:13
+    for j = 4:500
         w_testing(j) = U(:,j)'*A_training(:,i);
     end
     omega_testing(:,i) = w_testing;
@@ -112,6 +121,8 @@ hold off
 %test_img_set = randi(length(testing_set),5);
 num_correct = 0;
 percent_correct = 0.0;
+figure
+hold on
 for img = 1:length(testing_set)
     test_img = testing_set(:,:,img);
     for j = 1:length(omega_testing)
@@ -122,27 +133,26 @@ for img = 1:length(testing_set)
     % this returns the cropped_set_fixed index
     csf_idx = training_idx(p_index);
     face_num = char(face_buckets(csf_idx));
-    
     possible_matches = find(face_buckets == face_buckets(csf_idx));
     found_match = false;
     for i = 1:length(possible_matches)
         possible_idx = possible_matches(i);
         if (testing_set(:,:,img) == cropped_set_fixed(:,:,possible_idx))
-            disp(['Matching face found! Image ' num2str(img) ' ' num2str(possible_idx)])
+            disp(['Matching face found! Training image ' num2str(img) ' matches ' num2str(possible_idx) '. Predicted: ' num2str(p_index)])
             found_match = true;
             num_correct = num_correct + 1;
+            scatter(img,min(euclidian_arr),'.','g')
         end
     end
     if ~found_match
-        disp('No matching image found in predicted face')
+        disp(['No matching image found in predicted face. Closest match: image ' num2str(p_index)])
+        scatter(img,min(euclidian_arr),'.','r')
     end
+    
 end
+hold off
+xlabel('Test Face'); ylabel('Minimum Euclidean Distance');
+print(gcf,'distance','-dpng','-r300');
 
 percent_correct = num_correct / length(testing_set);
 disp(['Percent Correct ' num2str(percent_correct)])
-% randomly split into half for training and testing
-% get index of training set
-% divide index into faces
-% correspond new training set index with complete index
-% say which face it think it corresponds to
-% do difference on all the images from that face to confirm
